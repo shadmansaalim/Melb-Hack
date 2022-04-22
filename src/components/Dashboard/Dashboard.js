3344042ebdbbb16f365e1513ece77911fcc3c7f8
shadmansaalim321 <shadmansaalim321@gmail.com>
2022-04-23 02:11:27 +1000

    Unecessary files removed for clean code and spinner added in dashboard page to show while data loads

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -15,18 +15,30 @@ const Dashboard = () => {
 
     return (
         <div>
-            <Header></Header>
-            <Container className="my-5" >
-                <Row xs={1} md={2} lg={3} className="g-4 mx-auto container">
-                    {
-                        courses.map(course => <Course
-                            key={course._id}
-                            course={course}
-                        ></Course>)
-                    }
+            {
+                courses.length
+                    ?
+                    <div>
+                        <Header></Header>
+                        <Container className="my-5" >
+                            <Row xs={1} md={2} lg={3} className="g-4 mx-auto container">
+                                {
+                                    courses.map(course => <Course
+                                        key={course._id}
+                                        course={course}
+                                    ></Course>)
+                                }
 
-                </Row>
-            </Container>
+                            </Row>
+                        </Container>
+                    </div>
+                    :
+                    <div class="spinner d-flex align-items-center justify-content-center">
+                        <div class="bounce1"></div>
+                        <div class="bounce2"></div>
+                        <div class="bounce3"></div>
+                    </div>
+            }
         </div>
     );
 };
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,12 +7,12 @@ const Header = () => {
     return (
         <Navbar expand="lg">
             <Container>
-                <Navbar.Brand className="fw-bold" href="/">EModule</Navbar.Brand>
+                <Navbar.Brand className="fw-bold" href="/dashboard">EModule</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto d-flex align-items-center">
-                        <Nav.Link className="text-dark me-0 me-lg-2" href="#home">My Courses</Nav.Link>
-                        <Nav.Link className="text-dark me-0 me-lg-2" href="#link">Announcements</Nav.Link>
+                        <Nav.Link className="text-dark me-0 me-lg-2" href="/dashboard">My Courses</Nav.Link>
+                        <Nav.Link className="text-dark me-0 me-lg-2" href="#announcements">Announcements</Nav.Link>
                         <Nav.Link className="text-dark" href="#link">
                             <img src={profile} alt="" width="40" height="40" />
                         </Nav.Link>
