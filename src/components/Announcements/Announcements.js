9611e5982f7afa219a2bf9e7b8191df2ed76891d
mechetle <pigsky123@gmail.com>
2022-04-23 17:59:24 +1000

    Added students view of announcement page

--- src/components/Announcements/Announcements.js
+++ src/components/Announcements/Announcements.js
@@ -4,6 +4,27 @@ import { Row, Col, Container} from 'react-bootstrap';
 import './_announcements.scss';
 
 const Announcements = () => {
+    let data = {
+        announcements: [
+            {
+                date_published: null,
+                instuctor: "Instructor name",
+                title: "Please stop asking your teacher to go to the toilet during online class.",
+                body: "According to all known laws of aviation, there is no way that a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway.",
+            },
+            {
+                instuctor: "Instructor name",
+                title: "Please stop asking your teacher to go to the toilet during online class.",
+                body: "According to all known laws of aviation, there is no way that a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway.",
+            },
+            {
+                instuctor: "Instructor name",
+                title: "Please stop asking your teacher to go to the toilet during online class.",
+                body: "According to all known laws of aviation, there is no way that a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway.",
+            },
+        ]
+    }
+
     return (
         <div id="announcements">
             <Header></Header>
@@ -11,10 +32,24 @@ const Announcements = () => {
             <Container>
                 <h1>Announcements</h1>
 
-                <Row>
-                    <Col class="col-lg-6">
-                        
-                    </Col>
+                <Row className="gy-3 gx-3">
+                {data.announcements.map(function(object, i){
+                    return <Col className="col-6 col-lg-6 a-cards">
+                            <div className="card-wrap">
+                                <div className="a-heading">
+                                    <h2>
+                                        {object.title}
+                                    </h2>
+                                    <div class="instuctor">
+                                        <img src="https://picsum.photos/64" alt="" />
+                                        <h5>{object.instuctor}</h5>
+                                    </div>
+                                </div>
+                                <p><b>Posted: 1st April 2022</b></p>
+                                <p>{object.body}</p>
+                            </div>
+                        </Col>
+                    })}
                 </Row>
             </Container>
         </div>
